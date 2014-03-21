ee35f03754d7bce1c9f5208403326033fb73ae0b
Andy Ramakrishna <andyr.dev@gmail.com>
2014-03-20 22:51:40 -0700

    Adding design stubs for montessori assessment

---
+++ tmp-montessori-assessment/models.js
@@ -0,0 +1,25 @@
+// Outline of all model objects
+
+
+function Student() {}
+function Teacher() {}
+function Subject() {}
+
+
+
+// Work and WorkSet are really generic names, add specificity in the name
+// Curriculum?
+function WorkSet() {
+  // in-memory collection of work (what else?)
+  this.worksets;  // [{}]
+}
+
+
+function Work() {
+  this.id;
+  this.description;
+}
+
+
+function 
+
