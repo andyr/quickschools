46a6f1c0bcb85470417a62df9b89b0bfc9981e05
Andy Ramakrishna <andyr.dev@gmail.com>
2014-03-25 14:00:30 -0700

    fixed db manager class

--- tmp-montessori-assessment/DBState.js
+++ tmp-montessori-assessment/DBState.js
@@ -1,6 +1,6 @@
 function DBState() {
 
-  function deleteAllWorksets() {
+  function resetAllWorksets() {
     var ml = new MetisLoader('WorkSet');
     var worksets;
 
@@ -8,13 +8,25 @@ function DBState() {
       worksets = ml.getList();
       console.log('Trying to delete ', worksets.length, ' worksets: ', worksets);
       Metis.remove(worksets, this, function () { 
-        console.log("deleted: ", arguments); 
+        console.log("deleted: ", arguments);
+        var ml = new MetisLoader('Work');
+        Metis.load(ml, this, function () {
+          work = ml.getList();
+          Metis.remove(work, this, function () {
+            console.log('deleted: ', arguments);
+
+            console.log('adding workset...');
+            addWorkSet('Math', 'math work goes here', ['addition', 'subtraction']);
+            addWorkSet('English', 'English work goes here', ['literature', 'syntax']);
+          });
+        });
+
       });
     });
   }
 
   function createWorkSet(name, desc) {
-    var workset = new WorksetModel();
+    var workset = new WorkSetModel();
     workset.setName(name);
     workset.setDescription(desc);
     return workset;
@@ -28,24 +40,28 @@ function DBState() {
     return work;
   }
 
-  function addWorkSet(name, desc) {
+  function addWorkSet(name, desc, works) {
     var workset = createWorkSet(name, desc);
     Metis.save(workset, this, function () {
       console.log('saving workset with id:', workset.id, workset, arguments);
       
-      //console.log('addig work...'); // to the 'Work' table
-      //var work = createWork('addition');
-      //var work2 = createWork('subtraction');
-      //Metis.save([work1, work2], this, function () {
-      //  console.log('saving work to workset...');
-      //})
+      console.log('addig work...'); // to the 'Work' table
+      var workModels = [];
+      for(var i=0; i<works.length; i++) {
+        var workModel = new WorkModel();
+        workModel.setDescription(works[i]);
+        workModel.setSortOrder(i+1);
+        workModel.setWorkSetId(workset.id);
+        workModels.push(workModel);
+      }
+      Metis.save(workModels, this, function () {
+        console.log('saving work to workset...');
+      });
 
     });
   }
 
-  //(function setupWorkSets() {
-    deleteAllWorksets();
-    addWorkSet('Math', 'math work goes here');
-  //})();
+
+  resetAllWorksets();
 
 }
