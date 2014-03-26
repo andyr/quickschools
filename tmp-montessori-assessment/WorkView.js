a018eb1f0084da00467a5e46e63f9d28ceb9e768
Andy Ramakrishna <andyr.dev@gmail.com>
2014-03-26 12:12:26 -0700

    UI display fixes, made an edit button for consistency

--- tmp-montessori-assessment/WorkView.js
+++ tmp-montessori-assessment/WorkView.js
@@ -5,12 +5,15 @@ function WorkView(workset) {
 
   // render a filtered metis list for this work set id
   this.dialog = new FullPageDialog('Add/Edit Work');
+
+  new PageHeaderWidget(workset.name + " Work Items");
   new QuickAddButtonWidget('Add Work', this, 'clickedAddWork');
 
   this.workset = workset;
   var panel = new HorizontalPanelWidget("right", false);
   new DemotedButtonWidget("Close", this.dialog, "close");
   panel.finish();
+  new LineBreakWidget();
 
   this.dialog.resetInsertPosition();
 
@@ -22,6 +25,7 @@ function WorkView(workset) {
 WorkView.prototype.renderTable = function (worksetId) {
   var workTable = new DataTableWidget(this, 'workTable');
 
+  // TODO: remove this column when done testing
   workTable.addHeader('workSetId', 'workSetId', true);
   workTable.addColumn(function (work) {
     return work.getWorkSetId();
@@ -32,22 +36,35 @@ WorkView.prototype.renderTable = function (worksetId) {
     return work.getDescription();
   });
 
-  workTable.renderMetisData(Metis, 'Work', new EqFilter('workSetId', worksetId));
-  workTable.setClickHandler(this, function (work) {
-    console.log('Edit work', work);
-    var dialog = new EditWorkView(work);
-    dialog.setRefreshHandler(this, function () {
-      workTable.renderMetisData(Metis, 'Work', new EqFilter('workSetId', worksetId));
-    });
+  workTable.addHeader('', 'Actions');
+  workTable.addColumn(function (work) {
+    new ButtonWidget('Edit', this, 'clickedEditWork', work);
   });
+
+  workTable.renderMetisData(Metis, 'Work', new EqFilter('workSetId', worksetId));
+  // These rows aren't clickable (consistency)
+  //workTable.setClickHandler(this, function (work) {
+  //  console.log('Edit work', work);
+  //  var dialog = new EditWorkView(work);
+  //  dialog.setRefreshHandler(this, function () {
+  //    workTable.renderMetisData(Metis, 'Work', new EqFilter('workSetId', worksetId));
+  //  });
+  //});
 
   this.workTable = workTable;
 };
 
+WorkView.prototype.clickedEditWork = function (work) {
+  var dialog = new EditWorkView(work);
+  dialog.setRefreshHandler(this, function () {
+    this.workTable.renderMetisData(Metis, 'Work', new EqFilter('workSetId', work.workSetId));
+  });
+};
+
 WorkView.prototype.clickedAddWork = function () {
   console.log('clicked add work...');
-  var dialog = new EditWorkView();
 
+  var dialog = new EditWorkView();
   dialog.setRefreshHandler(this, function () {
     this.workTable.refreshTable();
   });
