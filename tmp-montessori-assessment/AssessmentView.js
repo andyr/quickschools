7eda0367ba5cbe69e0aabcc19a1da974cade299c
Andy Ramakrishna <andyr.dev@gmail.com>
2014-03-28 13:58:44 -0700

    Implemented AssessmentView as a component, dropdowns still appear in the wrong place

--- tmp-montessori-assessment/AssessmentView.js
+++ tmp-montessori-assessment/AssessmentView.js
@@ -1,5 +1,16 @@
 function AssessmentView() {
+  // This View extends Widget
+  ClassUtil.inherit(AssessmentView, this, Widget);
+  this._super('AssessmentView');
+
   new PageHeaderWidget('Assessment');
   new LineBreakWidget();
+
+  current = this.widget.find('section.teacher-subject');
   new TeacherSubjectView(); // delegate view
+
+  current = this.widget.find('section.student-notes');
+  new StudentAssessmentView();  // student table
+
+  this.attach();
 }
