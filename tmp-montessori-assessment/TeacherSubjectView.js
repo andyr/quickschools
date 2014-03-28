3e6338333bcc82f93b90548c7c5d4ce6baa41439
Andy Ramakrishna <andyr.dev@gmail.com>
2014-03-27 22:49:46 -0700

    Fixed initializing teacher-subject dropdowns; wip on change event error debugging

--- tmp-montessori-assessment/TeacherSubjectView.js
+++ tmp-montessori-assessment/TeacherSubjectView.js
@@ -3,6 +3,7 @@ function TeacherSubjectView() {
   console.log('Storage: ', Storage, Storage.get('userid'), Storage.get('teacher-subject'));
 
   new HeaderWidget('Current subject for ' + Storage.get('userid') + ': [selected subject]');
+  // Set up as a component and put these calls in separate containers
   new LineBreakWidget();
   // if not Storage.get(userId)==teacher-subject:
   //    if loggedInUser can see TeacherDropdown
@@ -23,43 +24,60 @@ TeacherSubjectView.prototype.renderTeacherDropdown = function () {
 };
 
 TeacherSubjectView.prototype.teacherDropdownCallback = function (pagedTeachers) {
+  var _this = this;
   this.teachers = pagedTeachers.list;
   this.panel = new QueryPanelWidget(250);
 
   this.queryFields = new QueryFields(this.panel); // don't use the initDynamicInfo arg, produces error
   this.panel.addLabel('Teachers');
-  this.queryFields.put('teacher', new DropDownWidget(this.teachers, 'id', 'fullName')); // prepend title in value
 
-  // assign id or class to the dropdown
-  // listen for changes and set the teacher id
-  this.renderSubjectDropDown(null); // set the teacherId here
+  this.teacherDropdown = new DropDownWidget(this.teachers, 'id', 'fullName')
+  this.queryFields.put('teacher', this.teacherDropdown); // prepend title in value
+  this.teacherDropdown.widget.on('change', {'scope': this}, this.renderSubjectDropDown);
+  this.renderSubjectDropDown();
 
-  this.panel.finish();
 };
 
-TeacherSubjectView.prototype.renderSubjectDropDown = function (teacherId) {
-  if( teacherId == null ) {
-    this.teacherId = this.queryFields.getValue('teacher');   // selected teacher
-  }
+TeacherSubjectView.prototype.renderSubjectDropDown = function (ev) {
+  console.log('renderSubjectDropDown this: ', this, ev); // pull out scope obj and get value from that
+  var scope = ev ? ev.data.scope : this;
+  //console.log('scope obj', scope)
+  this.teacherId = scope.queryFields.getValue('teacher');
   Rest.get('/sms/v1/sections', {}, this, this.subjectDropdownCallback);
 };
 
 TeacherSubjectView.prototype.subjectDropdownCallback = function (subjects) {
   var subjectsForTeacher = [];
-  console.log('collecting subjects for selected teacher = ', this.teacherId);
+  console.log('collecting subjects for selected teacher: ', this.teacherId);
 
   for(var i=0; i<subjects.length; i++) {
-    var teacherIds = $.map(subjects[i]['teachers'], function (teacher) { return teacher.id; });
-    if( $.inArray(this.teacherId, teacherIds) ) {
-      subjectsForTeacher.push( subjects[i].id );
+    var subject = subjects[i];
+    var teacherIds = $.map(subject['teachers'], function (teacher) { return teacher.id; });
+    if( $.inArray(this.teacherId, teacherIds) > -1 ) {
+
+      //subjectsForTeacher.push({ subject.id : subject.sectionName });
+      subjectsForTeacher.push({
+        'id': subject.id,
+        'name': subject.className + " " + subject.sectionName
+      });
     }
   }
 
   this.panel.addLabel('Subjects');
-  this.queryFields.put('subject', new DropDownWidget(subjectsForTeacher, 'id', 'values'));
+  if(this.subjectDropDown) {
+    this.subjectDropDown.widget.html('');
+  }
+  this.subjectDropDown = new DropDownWidget(subjectsForTeacher, 'id', 'name');
+  this.subjectDropDown.widget.on('change', this.changeSubjectCallback);
+  this.queryFields.put('subject', this.subjectDropDown);
 
+  this.panel.finish(); // serialize?
+};
+
+TeacherSubjectView.prototype.changeSubjectCallback = function () {
   // Listener for setting up selected teacher-subject
   // Select the DOM and listen for changes
   Storage.put('userid', 'asdf');
   Storage.put('teacher-subject', '');
+  console.log('changed subject, update the teacher-subject in Storage');
 };
