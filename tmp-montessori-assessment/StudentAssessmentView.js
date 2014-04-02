744b63cdfa3e0a91ede0aadf1ba42e82622d6535
Andy Ramakrishna <andyr.dev@gmail.com>
2014-04-01 17:50:54 -0700

    minor refactor; remove unnecessary method

--- tmp-montessori-assessment/StudentAssessmentView.js
+++ tmp-montessori-assessment/StudentAssessmentView.js
@@ -1,17 +1,35 @@
 function StudentAssessmentView() {
-  
+  // render a data table from /sms/v1/students
   new SpaceWidget(2);
   new HeaderWidget('Students');
   new LineBreakWidget();
-  // List of students
-  Rest.get('/sms/v1/students', {}, this, this.studentListingCallback);
 
-}
+  var studentTable = new DataTableWidget(this, 'studentTable');
+  this.studentTable = studentTable;
 
+  studentTable.addHeader('Id', 'id', true);
+  studentTable.addColumn(function (student) {
+    return student.id;
+  });
 
-StudentAssessmentView.prototype.studentListingCallback = function (pagedStudents) {
-  var students = pagedStudents.list;
-  console.log('students: ', students);
+  studentTable.addHeader('Name', 'fullName', true);
+  studentTable.addColumn(function (student) {
+    return student.fullName;
+  });
 
-  // render a data table
+  studentTable.setClickHandler(this, function (student) {
+    console.log('Clicked a student: ', student);
+
+    var dialog = new StudentWorkbook(student);
+    dialog.setRefreshHandler(this, function () {
+      this.studentTable.refreshTable();
+    });
+  });
+
+  studentTable.setRestLoader(function (settings, callback) {
+    console.log("in setRestLoader... ", settings, callback)
+    Rest.get('/sms/v1/students', settings, callback);
+  });
+  studentTable.render();
+
 };
