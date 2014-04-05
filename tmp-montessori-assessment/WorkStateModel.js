f3944c05682ec4a07cb793bbb5fa7f61733c45d0
Andy Ramakrishna <andyr.dev@gmail.com>
2014-04-04 19:17:25 -0700

    Added date formatter; getMonth returns 0-11 in this implementation

--- tmp-montessori-assessment/WorkStateModel.js
+++ tmp-montessori-assessment/WorkStateModel.js
@@ -10,3 +10,16 @@ function WorkStateModel() {
 Metis.define(WorkStateModel, 'WorkState', 'id', 'workId', 'studentId', 'state', 'date', 'comment');
 Metis.defineSortColumn(WorkStateModel, 'date', 'desc');
 Metis.createGettersAndSetters(WorkStateModel);
+
+WorkStateModel.prototype.getFormattedDate = function (format) {
+  var _this = this;
+  format = '%m/%d/%Y';
+  dateFormatMap = {
+    '%m': 'getMonth',
+    '%d': 'getDate',
+    '%Y': 'getFullYear'
+  };
+  return $.map(format.split('/'), function (token) {
+    return _this.date[dateFormatMap[token]]();
+  }).join('/');
+};
